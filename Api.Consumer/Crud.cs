5eef494e8c768d748572b29fd284d6b78a388787
YURAK SISAY SINCHICO ANRANGO <yssinchicoa@utn.edu.ec>
2026-06-02 12:16:20 -0500

    cambios de biblioteca de appi

---
+++ Api.Consumer/Crud.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Consumer
+{
+    public static  class Crud
+    {
+        public static void Create()
+        {
+
+        }
+
+        public static void Read()
+        {
+
+        }
+
+        public static void Update()
+        {
+
+        }
+
+        public static void Delete()
+        {
+
+        }
+    }
+}
