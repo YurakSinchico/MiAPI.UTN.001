2297753062e6861fdb9a0b93d606d689c27aa4b2
YURAK SISAY SINCHICO ANRANGO <yssinchicoa@utn.edu.ec>
2026-06-23 12:41:02 -0500

    seguro mvc y vistas index

---
+++ MiApp.MVC.Seguroo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiApp.MVC.Seguroo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
